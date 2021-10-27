7a61d7460629e32972f64cc2c0a68ec089a2f965
Coder Bravo <93136984+Coder-Bravo@users.noreply.github.com>
2021-10-27 23:31:14 +0530

    Create VowelOrConsonant.java

---
+++ VowelOrConsonant.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class VowelOrConsonant {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a character: ");
+        char ch = sc.next().charAt(0);
+
+        if(ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u' )
+            System.out.println(ch + " is vowel");
+        else
+            System.out.println(ch + " is consonant");
+    }
+}
