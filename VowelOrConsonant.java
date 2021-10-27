ccfe9829f651ba8aa9f8543fd06cf9ab987ca57a
Coder Bravo <93136984+Coder-Bravo@users.noreply.github.com>
2021-10-27 23:38:25 +0530

    Update VowelOrConsonant.java

--- VowelOrConsonant.java
+++ VowelOrConsonant.java
@@ -6,15 +6,29 @@ public class VowelOrConsonant {
         System.out.println("Enter a character: ");
 
         // using character
-//        char ch = sc.next().charAt(0);
+        char ch = sc.next().charAt(0);
 
         // using String and convert it into char
-        String str = sc.next();   //define String
-        char ch = str.charAt(0);  //reads a character from string at the specified index value
-
+//        String str = sc.next();   //define String
+//        char ch = str.charAt(0);  //reads a character from string at the specified index value
+        
+        // using if-else
         if(ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u' )
             System.out.println(ch + " is vowel");
         else
             System.out.println(ch + " is consonant");
+
+        // using switch-case
+//        switch (ch) {
+//            case 'a':
+//            case 'e':
+//            case 'i':
+//            case 'o':
+//            case 'u':
+//                System.out.println(ch + " is vowel");
+//                break;
+//            default:
+//                System.out.println(ch + " is consonant");
+        }
     }
 }
