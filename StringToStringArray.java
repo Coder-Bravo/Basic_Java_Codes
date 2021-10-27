69e46715bf7683c554e181b206f56dca50a31290
Coder Bravo <93136984+Coder-Bravo@users.noreply.github.com>
2021-10-27 09:48:14 +0530

    Update StringToStringArray.java

--- StringToStringArray.java
+++ StringToStringArray.java
@@ -1,14 +1,25 @@
+import java.util.*;
+
 public class StringToStringArray {
     public static void main(String[] args) {
+        // Method 1 (using String.split() method)
         //declaring and initializing a string
-        String str = "Converting string to string array using split() method";
-        //declaring an empty string array
-        String[] strArray = null;
-        //converting using String.split() method with whitespace as a delimiter
-        strArray = str.split(" ");
-        //printing the converted string array
-        for (int i = 0; i< strArray.length; i++){
-            System.out.println(strArray[i]);
-        }
+//        String str = "Converting string to string array using split() method";
+//        //declaring an empty string array
+//        String[] strArray = null;
+//        //converting using String.split() method with whitespace as a delimiter
+//        strArray = str.split(" ");
+//        //printing the converted string array
+//        for (int i = 0; i< strArray.length; i++){
+//            System.out.println(strArray[i]);
+//        }
+
+        // Method 2 (using String[] and .toString() method)
+        //declaring and initializing a string
+        String str = "Converting string to string array using String[]";
+        //passing the string to String[] {}
+        String[] strArray = new String[] {str};
+        //printing the string array using Arrays.toString()
+        System.out.println(Arrays.toString(strArray));
     }
 }
