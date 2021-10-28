53aeffe22c0b7df551f36150e35e75ca8d6b0ebf
Coder Bravo <93136984+Coder-Bravo@users.noreply.github.com>
2021-10-29 01:04:56 +0530

    Last update using StringBuilder

--- StringReverse.java
+++ StringReverse.java
@@ -1,23 +1,30 @@
 public class StringReverse {
     public static void main(String[] args) {
-        String mystring = "My name is Pratik";
-        String rev = "";
-        // Method 1 (using regular method)
+       String mystring = "My name is Pratik";
+       String rev = "";
+        
+       // Method 1 (using regular method)
        int n = mystring.length();
        for (int i = n-1; i>=0; i--) {
            rev = rev + mystring.charAt(i);
        }
+       System.out.println(rev);
 
-        // Method 2 (using char[])
+       // Method 2 (using char[])
 //        char[] chararray = mystring.toCharArray();
 //        int n = chararray.length;
 //        for (int i = n-1; i>=0; i--) {
 //            rev = rev + chararray[i];
 //        }
+//        System.out.println(rev);
 
-        // Method 3 (using StringBuffer)
-//         StringBuffer sbf = new StringBuffer(mystring);
-//         System.out.println(sbf.reverse());
-//         System.out.println(rev);
+       // Method 3 (using StringBuffer)
+//        StringBuffer sbf = new StringBuffer(mystring);
+//        System.out.println(sbf.reverse());
+        
+       // Method 4 (using StringBuilder)
+//        StringBuilder sbl = new StringBuilder(mystring); 
+//        System.out.println("string = " + sbl);
+//        System.out.println("reverse = " + sbl.reverse()); 
     }
 }
