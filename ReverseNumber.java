5d65d9d7438896e72985124a84552c765747c17e
Coder Bravo <93136984+Coder-Bravo@users.noreply.github.com>
2021-10-29 12:27:14 +0530

    Update and rename IntRev.java to ReverseNumber.java

--- ReverseNumber.java
+++ ReverseNumber.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class IntRev {
+public class ReverseNumber {
     public static void main(String[] args) {
         Scanner sc  = new Scanner(System.in);
         System.out.print("Enter a number: ");
